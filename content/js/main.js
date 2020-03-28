b6d7211a804cab16e2a1a4f79cfe3986aefe5a7b
Frederik B <freddyb@users.noreply.github.com>
2020-03-28 22:00:45 +0100

    stop using requestAnimation frame for css

--- content/js/main.js
+++ content/js/main.js
@@ -12,19 +12,3 @@ window.addEventListener("load", function() {
     node.innerHTML = "<a href='mailto:" + e + "'>" + e + "</a>";
   }
 });
-// load bigger styles later
-// (so pygment don't block page load)
-var loadStyles = function() {
-  var lcode = document.createElement("link");
-  lcode.rel = "stylesheet";
-  lcode.href = "/theme/css/pygment.css"; // code highlighting
-  var h = document.getElementsByTagName("head")[0];
-  h.parentNode.insertBefore(lcode, h);
-};
-var raf =
-  requestAnimationFrame ||
-  mozRequestAnimationFrame ||
-  webkitRequestAnimationFrame ||
-  msRequestAnimationFrame;
-if (raf) raf(loadStyles);
-else window.addEventListener("load", loadStyles);
