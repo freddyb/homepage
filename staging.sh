28bdf568fa835072d37e5b05523bdf2536b24083
Frederik B <freddyb@users.noreply.github.com>
2020-04-20 20:35:30 +0200

    making the build a bit more testable/reproducible

---
+++ staging.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export LANG=en_US.UTF-8
+source venv/bin/activate
+
+rm -r output-staging/*
+pelican content/ -o output-staging/ -s stagingconf.py 
+rsync -v -r output-staging/ fbcom@kushida.uberspace.de:/var/www/virtual/fbcom/http09.on.web.security.plumbing/
